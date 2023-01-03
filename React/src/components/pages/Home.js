778b9b45a5cf66e1030a599195a815f5b2c5da76
ewa-markowska <98181109+ewa-markowska@users.noreply.github.com>
2023-01-03 15:19:31 +0100

    Create Home.js

---
+++ React/src/components/pages/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../../App.css';
+import MainView from '../MainView';
+
+function Home(){
+    return(
+        <>
+        <MainView/>
+        </>
+    );
+}
+export default Home;
